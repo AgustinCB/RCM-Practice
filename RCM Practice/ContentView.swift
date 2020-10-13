34381b746d5be1d31f85301cbc9765a3826e03ab
Agustin Chiappe Berrini <agustin@chiappeberrini.me>
2020-10-13 12:41:00 -0700

    Initial Commit

---
+++ RCM Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RCM Practice
+//
+//  Created by Agustin Chiappe Berrini on 2020-10-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RCM Practice/RCM_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RCM_PracticeApp.swift
+//  RCM Practice
+//
+//  Created by Agustin Chiappe Berrini on 2020-10-13.
+//
+
+import SwiftUI
+
+@main
+struct RCM_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
