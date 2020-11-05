858aafe73e61b8929670115182c0f14a64447e29
Agustin Chiappe Berrini <agustin@chiappeberrini.me>
2020-11-04 17:43:02 -0800

    Create basic user interface

--- RCM Practice/ContentView.swift
+++ RCM Practice/ContentView.swift
@@ -7,10 +7,66 @@
 
 import SwiftUI
 
+let WELLCOME_MESSAGE = "Hey there! Guess the sequence!"
+let PLAYING_SEQUENCE = "Playing the Sequence..."
+let MAJOR_THIRD = "Guessed Major Third"
+let MINOR_THIRD = "Guessed Minor Third"
+let CORRECT_GUESS = "You guessed correctly! Hurray!"
+let INCORRECT_GUESS = "You guessed wrongly! Looser!"
+
+enum Note: UInt8, CaseIterable {
+    case MajorThird = 4
+    case MinorThird = 3
+}
+
 struct ContentView: View {
+    @State var score: Int = 0
+    @State var message: String = WELLCOME_MESSAGE
+    @State var intervalPlayer: IntervalPlayer! = IntervalPlayer.init(root: UInt8.random(in: 60...71), interval: Note.allCases.randomElement()!.rawValue)
+    
+    fileprivate func processGuess(note: Note, resetAction: (String) -> Void) {
+        var result: String = ""
+        if self.intervalPlayer.interval == note.rawValue {
+            self.score += 1
+            result = CORRECT_GUESS
+        } else {
+            result = INCORRECT_GUESS
+        }
+        resetAction(result)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        let resetAction: (String) -> Void = { message in
+            self.message = message
+            self.intervalPlayer.setInterval(root: UInt8.random(in: 60...71), interval: Note.allCases.randomElement()!.rawValue)
+        }
+        VStack {
+            Text("\(self.message)")
+                .padding()
+            Button("Play Sequence", action: {
+                self.message = PLAYING_SEQUENCE
+                self.intervalPlayer.playInterval()
+            })
+            .padding(.bottom)
+            HStack {
+                Button("Guess Major Third", action: {
+                    self.message = MAJOR_THIRD
+                    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5)) {
+                        processGuess(note: Note.MajorThird, resetAction: resetAction)
+                    }
+                })
+                .padding(.trailing)
+                Button("Guess Minor Third", action: {
+                    self.message = MINOR_THIRD;
+                    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5)) {
+                        processGuess(note: Note.MinorThird, resetAction: resetAction)
+                    }
+                })
+                .padding(.leading)
+            }
+            Text("Your score is \(self.score)")
+                .padding(.top)
+        }
     }
 }
 
---
+++ RCM Practice/IntervalPlayer.swift
@@ -0,0 +1,78 @@
+//
+//  IntervalPlayer.swift
+//  RCM Practice
+//
+//  Created by Agustin Chiappe Berrini on 2020-10-26.
+//
+
+import AVFoundation
+import Foundation
+
+let INTERVAL_MIDI_HEADER: [UInt8] = [
+    0x4D, 0x54, 0x68, 0x64, // MIDI header: MThd
+    0x00, 0x00, 0x00, 0x06, // MIDI header length: 6 bytes
+    0x00, 0x00, // Single channel
+    0x00, 0x01, // Number of channels
+    0x00, 0x01, // Number of ticks per beat: 1 tick per beat
+    0x4D, 0x54, 0x72, 0x6b, // Track header: MThd
+]
+
+let INTERVAL_MIDI_TRACK_METADATA: [UInt8] = [
+    0x00, 0x00, 0x00, 0x3A, // Track length: 4 bytes * 8 events + 4 bytes for end of header + 22 for the headers = 58
+    0x00, 0xFF, 0x58, 0x04, // Specify time signature, and clarify that the next four bytes will contain that information
+    0x04, 0x04, 0x18, 0x08, // Time signature is 4/4, and 0x18 MIDI clocks in a metronome clock
+    0x00, 0xFF, 0x51, 0x03, // Specify the tempo of the track.
+    0x08, 0x7a, 0x23, // 1,000,000 microseconds per quarter note. Because it is in 4/4, this means 60 bpm.
+    0x00, 0xc0, 0x4f, // Set MIDI instrument to 79 (Flute)
+    0x00, 0xb0, 0x07, 0x20, // Main volume
+]
+
+let END_OF_TRACK_BYTES: [UInt8] = [0x00,0xff,0x2f,0x00]
+
+struct IntervalPlayer {
+    var midiPlayer: AVMIDIPlayer?
+    var root: UInt8!
+    var interval: UInt8!
+    
+    init(root: UInt8, interval: UInt8) {
+        self.setInterval(root: root, interval: interval)
+    }
+    
+    mutating func setInterval(root: UInt8, interval: UInt8) {
+        self.root = root
+        self.interval = interval
+        let intervalNote = root + interval
+        let sequence: [UInt8] = INTERVAL_MIDI_HEADER + INTERVAL_MIDI_TRACK_METADATA + [
+            0x00, 0x90, root, 0x40, // Play the root at beat 1.
+            0x01, 0x80, root, 0x00, // Stop playing the root at beat 2.
+            0x00, 0x90, intervalNote, 0x40, // Play the interval at beat 2.
+            0x01, 0x80, intervalNote, 0x00, // Stop playing the interval at beat 3.
+            0x01, 0x90, intervalNote, 0x40, // Play the interval at beat 4.
+            0x01, 0x80, intervalNote, 0x00, // Stop playing the interval at beat 5.
+            0x00, 0x90, root, 0x40, // Play the root at beat 5.
+            0x01, 0x80, root, 0x00, // Stop playing the root at beat 6.
+        ] + END_OF_TRACK_BYTES
+        let data = Data.init(sequence)
+        
+        guard let bankURL = Bundle.main.url(forResource: "FluidR3_GM", withExtension: "sf2") else {
+            fatalError("soundbank file not found.")
+        }
+        do {
+            self.midiPlayer = try AVMIDIPlayer.init(data: data, soundBankURL: bankURL)
+            print("created midi player with sound bank url \(bankURL)")
+            self.midiPlayer!.prepareToPlay()
+            print("PREPARES TO PLAY")
+        } catch let error as NSError {
+            print("Error \(error.localizedDescription)")
+        }
+    }
+    func playInterval() {
+        if !self.midiPlayer!.isPlaying {
+            self.midiPlayer!.currentPosition = 0
+            self.midiPlayer!.play({
+                print("FINISHED")
+            })
+            print("PLAYS?")
+        }
+    }
+}
